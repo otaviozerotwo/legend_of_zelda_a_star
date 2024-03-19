b9b04df83a88b45d53da85486fa45cf860dc40b0
Otavio Cardoso <otavio.cardoso@rfn.ptc>
2024-03-19 18:33:02 -0300

    refactor: implementação componente para cálculo da heurística

--- src/components/MapaHyrule.js
+++ src/components/MapaHyrule.js
@@ -1,18 +1,33 @@
+import React, { useState } from 'react';
 import Celula from './Celula';
+import calcularDistanciaEuclidiana from './Heuristica';
 
 import '../styles/MapaHyrule.css';
 
 const MapaHyrule = ({ matrizMapaHyrule }) => {
+  const [posicaoAtual, setPosicaoAtual] = useState({ x: 25, y: 28 });
+
   return (
     <div className="mapa-hyrule-container">
       {matrizMapaHyrule.map((row, rowIndex) => (
         <div key={rowIndex} className="mapa-linha">
-          {row.map((cell, colIndex) => (
-            <Celula
-              key={colIndex}
-              value={cell}
-            />
-          ))}
+          {row.map((cell, colIndex) => {
+            const heuristica = calcularDistanciaEuclidiana(
+              posicaoAtual.x,
+              posicaoAtual.y,
+              rowIndex,
+              colIndex
+            );
+
+            const custoComHeuristica = cell + heuristica;
+
+            return (
+              <Celula
+                key={colIndex}
+                value={custoComHeuristica}
+              />
+            );
+          })}
         </div>
       ))}
     </div>
