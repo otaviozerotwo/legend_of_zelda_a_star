57f0ce08d6d033d9a980a500d37e8325a6136152
otaviozerotwo <deadplatypus@protonmail.com>
2024-03-24 16:52:05 -0300

    refactor: implementada lógica para voltar no caminho quando entrar em beco sem saída

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './styles/MapaHyrule.css';
+import "./styles/MapaHyrule.css";
 
 const matrizMapaHyrule = [
   [100, 10, 10, 10, 10, 0, 10, 10, 10, 100],
@@ -12,19 +12,27 @@ const matrizMapaHyrule = [
   [100, 10, 10, 10, 10, 10, 10, 100, 10, 10],
   [100, 10, 100, 100, 100, 100, 100, 100, 100, 10],
   [100, 10, 10, 10, 10, 10, 10, 100, 10, 10],
-  [100, 10, 100, 100, 100, 100, 100, 100, 100, 10]
+  [100, 10, 100, 100, 100, 100, 100, 100, 100, 10],
 ];
 
 const MapaHyrule = ({ matrizMapaHyrule }) => {
   const [posicaoAtual, setPosicaoAtual] = useState([4, 5]); // Inicializando na posição [4, 5]
   const [visitado, setVisitado] = useState(Array.from({ length: matrizMapaHyrule.length }, () => Array.from({ length: matrizMapaHyrule[0].length }, () => false)));
+  const [caminhoSemSaida, setCaminhoSemSaida] = useState(false);
+  const [pilhaPosicoes, setPilhaPosicoes] = useState([]);
 
   useEffect(() => {
-    const pontoFim = [9, 9];
+    const pontoFim = [9, 0];
 
     const intervalId = setInterval(() => {
       setPosicaoAtual(prevPosicao => {
         const [linhaAtual, colunaAtual] = prevPosicao;
+
+        // Verificar se já chegamos ao ponto de destino
+        if (linhaAtual === pontoFim[0] && colunaAtual === pontoFim[1]) {
+          clearInterval(intervalId);
+          return prevPosicao; // Retornar a posição atual, pois já estamos no destino
+        }
 
         // Função para calcular o custo de movimento para uma posição
         const calcularCusto = (linha, coluna) => {
@@ -42,49 +50,52 @@ const MapaHyrule = ({ matrizMapaHyrule }) => {
           [linhaAtual, colunaAtual - 1]  // Esquerda
         ];
 
-        // Verificar se a posição atual já é a posição de destino
-        if (linhaAtual === pontoFim[0] && colunaAtual === pontoFim[1]) {
-          clearInterval(intervalId);
-          return prevPosicao; // Retornar a posição atual, pois já estamos no destino
-        }
-
         // Encontrar a posição vizinha com menor custo
         let menorCusto = Infinity;
         let novaPosicao = prevPosicao;
-        vizinhos.forEach(([linha, coluna]) => {
-          const custoVizinho = calcularCusto(linha, coluna);
-          if ((!visitado[linha] || !visitado[linha][coluna]) && custoVizinho < menorCusto) {
-            menorCusto = custoVizinho;
-            novaPosicao = [linha, coluna];
-          } 
-        });
+        let encontrouVizinho = false;
 
+        for (const [linha, coluna] of vizinhos) {
+          if (linha >= 0 && linha < matrizMapaHyrule.length && coluna >= 0 && coluna < matrizMapaHyrule[0].length && !visitado[linha][coluna]) {
+            const custoVizinho = calcularCusto(linha, coluna);
+            if (custoVizinho < menorCusto) {
+              menorCusto = custoVizinho;
+              novaPosicao = [linha, coluna];
+              encontrouVizinho = true;
+            }
+          }
+        }
+        
         // Marcar a posição atual como visitada
         setVisitado(prevVisitado => {
           const novoVisitado = prevVisitado.map((linha, indexLinha) =>
             indexLinha === linhaAtual ? { ...linha, [colunaAtual]: true } : linha
           );
-
-          console.log('Array visitados: ', novoVisitado);
-
           return novoVisitado;
         });
 
-        // Verificar se não há mais movimentos possíveis
-        const semMovimentos = vizinhos.every(([linha, coluna]) => calcularCusto(linha, coluna) === Infinity);
-
-        if ((novaPosicao[0] === pontoFim[0] && novaPosicao[1] === pontoFim[1]) || semMovimentos) {
-          clearInterval(intervalId);
+        // Verificar se estamos em um beco sem saída
+        if (!encontrouVizinho) {
+          setCaminhoSemSaida(true);
+          if (pilhaPosicoes.length > 0) {
+            // Retroceder para a última posição não visitada
+            const ultimaPosicao = pilhaPosicoes.pop();
+            setPosicaoAtual(ultimaPosicao);
+          }
+        } else {
+          setCaminhoSemSaida(false);
+          // Armazenar a posição atual na pilha
+          setPilhaPosicoes([...pilhaPosicoes, prevPosicao]);
         }
 
-        console.log('Nova Posição: ', novaPosicao);
+        console.log(novaPosicao);
 
         return novaPosicao;
       });
     }, 500);
 
     return () => clearInterval(intervalId);
-  }, [matrizMapaHyrule, visitado]);
+  }, [matrizMapaHyrule, pilhaPosicoes, visitado]);
 
   // Renderização da matriz na tela
   return (
@@ -92,13 +103,23 @@ const MapaHyrule = ({ matrizMapaHyrule }) => {
       {matrizMapaHyrule.map((row, rowIndex) => (
         <div key={rowIndex} className="mapa-linha">
           {row.map((cell, colIndex) => {
-            const posicaoAtualValida = posicaoAtual[0] === rowIndex && posicaoAtual[1] === colIndex;
-            const visitadoValido = visitado[rowIndex] && visitado[rowIndex][colIndex];
+            const posicaoAtualValida =
+              posicaoAtual[0] === rowIndex && posicaoAtual[1] === colIndex;
+            const visitadoValido =
+              visitado[rowIndex] && visitado[rowIndex][colIndex];
             return (
-              <div key={colIndex} style={{ backgroundColor: posicaoAtualValida ? 'yellow' : visitadoValido ? 'gray' : 'transparent' }}
-                className='mapa-celula'
+              <div
+                key={colIndex}
+                style={{
+                  backgroundColor: posicaoAtualValida
+                    ? "yellow"
+                    : visitadoValido
+                    ? "gray"
+                    : "transparent",
+                }}
+                className="mapa-celula"
               >
-                {cell}
+                {caminhoSemSaida && posicaoAtual[0] === rowIndex && posicaoAtual[1] === colIndex ? 'X' : cell}
               </div>
             );
           })}
@@ -109,11 +130,7 @@ const MapaHyrule = ({ matrizMapaHyrule }) => {
 };
 
 function App() {
-  return (
-    <MapaHyrule 
-      matrizMapaHyrule={matrizMapaHyrule} 
-    />
-  );
+  return <MapaHyrule matrizMapaHyrule={matrizMapaHyrule} />;
 }
 
 export default App;
